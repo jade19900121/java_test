b2a76a4c16bbd4c3bdf7340657bbb961adcd9b49
jade19900121 <289572804@qq.com>
2020-11-14 12:46:37 +0800

    (1)第一次提交代码

---
+++ src/hello_world.java
@@ -0,0 +1,9 @@
+/**
+ * @author fangjiulin
+ * @date 2020/11/14 12:40
+ */
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("hello_world");
+    }
+}
