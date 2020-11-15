4e0869795607b058a0a2ff39b821758c992a6f7e
jade19900121 <289572804@qq.com>
2020-11-15 21:42:08 +0800

    (1)添加最新代码

--- src/hello_world.java
+++ src/hello_world.java
@@ -5,6 +5,14 @@
 public class hello_world {
     //这里是test_1分支的代码
     public static void main(String[] args) {
-        System.out.println("hello_world");
+        //System.out.println("hello_world");
+        Run a = new Run();
+        a.run();
+    }
+}
+
+class Run{
+    void run(){
+        System.out.println("i am running");
     }
 }
